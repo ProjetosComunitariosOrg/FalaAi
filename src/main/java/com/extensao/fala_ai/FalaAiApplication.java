76e78fd3c7628bb040099cc006fce363a1f5c0b8
Hendrick Miqueias <hendrickmiqueias@Hotmail.com>
2026-03-23 13:52:00 -0300

    Projeto Inicial Criado

---
+++ src/main/java/com/extensao/fala_ai/FalaAiApplication.java
@@ -0,0 +1,13 @@
+package com.extensao.fala_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/extensao/fala_ai/FalaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extensao.fala_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
