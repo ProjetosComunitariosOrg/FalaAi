3da75b234bf87c20c12a17da8b6198c0aa111854
Hendrick Miqueias <hendrickmiqueias@Hotmail.com>
2026-03-25 20:35:43 -0300

    Adicionado bean validator ao projeto e implementado no UserRequestDTO

--- src/main/java/com/extensao/fala_ai/dto/UserRequestDTO.java
+++ src/main/java/com/extensao/fala_ai/dto/UserRequestDTO.java
@@ -1,11 +1,23 @@
 package com.extensao.fala_ai.dto;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record UserRequestDTO(
+	@NotBlank(message =  "O nome não pode estar em branco!")
     @Schema(example = "Joao Silva") String name,
+    @NotBlank
+    @CPF(message = "CPF inválido")
     @Schema(example = "12345678901") String cpf,
+    @NotBlank
+    @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
     @Schema(example = "senha123") String password,
+    @NotBlank
+    @Pattern(regexp = "\\d{11}", message = "O telefone deve ter 11 dígitos")
     @Schema(example = "34912345678") String phone
 ) {
 }
