4c38a97682c12393e04f7603fef3e8b9897d9f4f
luis-otavio-dias <luis777dias231@gmail.com>
2026-05-04 17:54:29 -0300

    Adiciona assets png para página de Login

--- frontend/src/declarations.d.ts
+++ frontend/src/declarations.d.ts
@@ -1,6 +1,12 @@
 declare module "*.css";
+declare module "*.png";
 
 declare module "*.module.css" {
   const classes: { readonly [key: string]: string };
   export default classes;
+}
+
+declare module "*.module.png" {
+  const src: string;
+  export default src;
 }
